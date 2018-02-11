42f75eae631efbce9a8caa7269afe14f0c94aa9d
david <d.marsalone@gmail.com>
2018-02-11 12:48:49 +0100

    Install test

--- Pov/Installer.php
+++ Pov/Installer.php
@@ -5,6 +5,10 @@ namespace Pov;
 
 class Installer
 {
+    /**
+     * Installe ce qu'il faut pour que le FMK fonctionne, à savoir principalement un fichier index.php et un fichier .htaccess.
+     * Si les fichiers existent déjà, ils ne sont pas écrasés.
+     */
     public static function install(){
 
         //copier le .htaccess à la racine
@@ -16,6 +20,12 @@ class Installer
             copy(__DIR__."/../_install/index.php","index.php");
         }
         //crée le répertoire/fichier configs/routes/127.0.0.1/mon-projet.php
+        if(!is_dir("configs/routes/127.0.0.1")){
+            mkdir("configs/route/127.0.0.1",0777,true);
+        }
+        if(!is_dir("files")){
+            mkdir("files");
+        }
 
 
     }
