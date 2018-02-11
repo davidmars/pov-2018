a0c6a50c5a05d393f71f67da3653112654e2638d
david <d.marsalone@gmail.com>
2018-02-11 12:21:28 +0100

    Install test

--- Pov/Installer.php
+++ Pov/Installer.php
@@ -10,5 +10,16 @@ class Installer
         //on crée un fichier ici pour tester
         file_put_contents("salut-$timestring.txt","tesssst\n$timestring\n".getcwd());
 
+        //copier le .htaccess à la racine
+        if(!file_exists(".htaccess")){
+            copy(__DIR__."/../_install/.htaccess",".htaccess");
+        }
+        //copie le fichier index.php à la racine
+        if(!file_exists("index.php")){
+            copy(__DIR__."/../_install/index.php","index.php");
+        }
+        //crée le répertoire/fichier configs/routes/127.0.0.1/mon-projet.php
+
+
     }
 }
