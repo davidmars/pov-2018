5a716162ae172f72de82891d189ace8bfdc2dfb9
david <d.marsalone@gmail.com>
2018-02-12 11:35:58 +0100

    uikit icons

--- _src/dev-ui.js
+++ _src/dev-ui.js
@@ -3,4 +3,11 @@
  */
 require("uikit/dist/css/uikit.min.css");
 require("uikit/dist/js/uikit.min");
+
+import UIkit from 'uikit';
+import Icons from 'uikit/dist/js/uikit-icons';
+window.UIkit = UIkit;
+// loads the Icon plugin
+UIkit.use(Icons);
+
 require("./pov.boot");
